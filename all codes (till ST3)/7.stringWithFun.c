f47d32d93e173e9a2b305f9f5f6a34cb1d4c02ca
Yashasvi <103374718+yashasvi242@users.noreply.github.com>
2023-02-23 12:00:08 +0530

    Create 7.stringWithFun.c

---
+++ all codes (till ST3)/7.stringWithFun.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+
+// NULL character & lenght of character string.
+
+int main(){
+    
+    char str[] = "abcdefghij";
+    
+    puts(str);
+    int size = sizeof(str)/sizeof(char);
+    printf("length of string : %d\n", size);
+    
+    // although their are 10 characters in string, but output is 11 coz here last element is '\0'
+    char ch = str[10];
+    printf("%c",ch); // this will print empty line coz at 10th index '\0' is present
+    
+    // null check 
+    if(str[10] == '\0') printf("null char found");
+}
+
+/* Output :- 
+
+    abcdefghij
+    length of string : 11
+    null char found
+    
+*/
